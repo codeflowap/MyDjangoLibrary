380f34c4ede5cff8c99e8e4be292b1c900bdffff
Ahmad Pour <dram62@gmail.com>
2020-05-05 00:43:48 +0300

    admin.py updated

---
+++ mydjanglibrary/mydjanglibrary/apps/author/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from library_project.apps.author.models import Author
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'get_author_full_name',
+        'user',
+        'gender',
+        'birth_date',
+        'created_at',
+        'updated_at',
+    )
+
+    def get_author_full_name(self, obj):
+        first_name = obj.user.first_name
+        last_name = obj.user.last_name
+
+        return f'{first_name} {last_name}'
+
+    get_author_full_name.short_description = 'Author'
+
